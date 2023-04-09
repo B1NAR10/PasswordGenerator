89076f956b4c6cc3f242c637a024120bc4d0da15
Alberto <amtaboada99@gmail.com>
2023-04-09 09:38:27 +0200

    Translations have been added for the following languages: Galician (gl), Catalan (ca), Basque (eu), Portuguese (pt), French (fr) and German (de)

--- generator.py
+++ generator.py
@@ -1,14 +1,49 @@
 import random
 import string
-import gettext
 
-numero_caracteres = int(input("Introduzca Cantos Caracteres quere que teña: "))
+# Definir as Mensaxes en diferentes Idiomas
+messages = {
+    'password_length': {
+        'en': 'Enter how many characters you want the Password to be:',
+        'es': 'Introduce la longitud deseada de la Contraseña: ',
+        'gl': 'Introduce a Lonxitude desexada do Contrasinal: ',
+        'ca': 'Introdueix la Longitud desitjada de la Contrasenya: ',
+        'eu': 'Sartu Pasahitzaren luzera nahi Duzuna: ',
+        'pt': 'Insira o comprimento desejado da Senha: ',
+        'fr': 'Entrez la longueur souhaitée du mot de Passe : ',
+        'de': 'Geben Sie die gewünschte Länge des Passworts ein: '
+    },
+    'password': {
+        'en': 'Password: ',
+        'es': 'Contraseña: ',
+        'gl': 'Contrasinal: ',
+        'ca': 'Contrasenya: ',
+        'eu': 'Pasahitza: ',
+        'pt': 'Senha: ',
+        'fr': 'Mot de Passe: ',
+        'de': 'Passwort: '
+    }
+}
 
+# Función para obter a traducción dun mensaxe nun idioma determinado
+def gettext(message, lang):
+    return messages.get(message, {}).get(lang, message)
+
+# Solicitar o Idioma desexado polo Usuario
+lang = input('Choose your language (en/es/gl/ca/eu/pt/fr/de): ')
+
+# Solicitar a Lonxitude da Contrasinal ao Usuario
+numero_caracteres = int(input(gettext('password_length', lang)))
+
+# Definir os Caracteres que se utilizarán para Xerar a Contrasinal
 minusculas  = string.ascii_lowercase
 maiusculas  = string.ascii_uppercase
 numeros     = string.digits
 simbolos    = string.punctuation
 caracteres  = minusculas + maiusculas + numeros + simbolos
 
-xerador     = random.sample(caracteres, numero_caracteres)
-print("Contrasinal: " + "".join(xerador))
+# Xerar unha Contrasinal Aleatoria
+xerador = random.sample(caracteres, numero_caracteres)
+
+# Imprimir a Contrasinal no Idioma Seleccionado
+print(gettext('password', lang) + "".join(xerador))
